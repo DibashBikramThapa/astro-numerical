3da3735cff1f950a3bd75a2abb78a29c81cd4704
Dibash Bkram Thapa <dwasthapa172.com>
2023-11-16 19:59:20 +0545

    Setup
     - [x] Layout, navbar,footer,logo,bk-images

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function footer() {
+    return (
+        <footer>
+            Copyright 2023© AstroNumeral. All Rights Reserved.
+        </footer>
+    )
+}
---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+export default function navbar() {
+    return (
+        <div>
+            <img src={'/images/astro-logo.png'}/>
+            <div>
+                <h4>AstroNumeral</h4>
+                <p>Discover Your Cosmic Code, Illuminate Your Path.</p>
+            </div>
+        </div>
+    )
+}
